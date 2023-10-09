7be2847c86d077ff0125e56370a10ee09873649a
Pau <pauete.sb@gmail.com>
2023-10-10 01:48:47 +0200

    chestloot command added, similar to /reload, reloads data from datapacks

--- src/main/java/com/fadedbytes/CofreTiquismiquis.java
+++ src/main/java/com/fadedbytes/CofreTiquismiquis.java
@@ -5,10 +5,15 @@ import com.fadedbytes.data.LootTableSelector;
 import com.fadedbytes.data.LootTableSelectorDeserializer;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.resource.ResourceType;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ReloadCommand;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +23,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import static net.minecraft.server.command.CommandManager.literal;
+
 public class CofreTiquismiquis implements ModInitializer {
 	public static final String MOD_ID = "cofretiquismiquis";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -25,6 +32,41 @@ public class CofreTiquismiquis implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		this.registerChestlootCommand();
+		this.registerDataReloader();
+
+	}
+
+	private void registerChestlootCommand() {
+		CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(
+					literal("chestloot")
+							.executes(context -> {
+								context.getSource().sendMessage(Text.literal("Usa /chestloot reload para recargar las loot tables de cofres."));
+
+								return 0;
+							})
+							.requires(source -> source.hasPermissionLevel(3))
+							.then(literal("reload")
+									.executes(context -> {
+										context.getSource().sendMessage(Text.literal("Recargando loot tables de cofres..."));
+
+										ResourcePackManager manager = context.getSource().getServer().getDataPackManager();
+										manager.scanPacks();
+
+										context.getSource().getServer().reloadResources(manager.getEnabledNames()).exceptionally((exception) -> {
+											context.getSource().sendError(Text.literal("Error recargando loot tables de cofres: " + exception.getMessage()));
+											return null;
+										});
+
+										return 1;
+									})
+							)
+			);
+		}));
+	}
+
+	private void registerDataReloader() {
 		/*
 		 "By all laws of programming, you should not be able to instantiate an interface in java,
 		 its methods are too abstract to call.
@@ -32,7 +74,8 @@ public class CofreTiquismiquis implements ModInitializer {
 
 		  -- Fabric documentation
 		 */
-		ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
+
+		SimpleSynchronousResourceReloadListener lootTableSelectorLoader = new SimpleSynchronousResourceReloadListener() {
 			@Override
 			public Identifier getFabricId() {
 				return new Identifier(MOD_ID, "loot_table_selector");
@@ -46,17 +89,18 @@ public class CofreTiquismiquis implements ModInitializer {
 				).forEach((identifier, resource) -> {
 					LOGGER.debug("Found loot table selector: " + identifier.toString());
 
-					try {
-						String json = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+					try (InputStream stream = resource.getInputStream()) {
+						String json = new String(stream.readAllBytes(), StandardCharsets.UTF_8);
 						LootTableSelector lootTableSelector = LootTableSelectorDeserializer.readLootTableSelector(json);
 
 						BiomedChestContent.setSelector(lootTableSelector);
 					} catch (IOException e) {
-						LOGGER.error("Error reading loot table selector: " + identifier.toString());
+						LOGGER.error("Error reading loot table selector: " + identifier);
 					}
 				});
 			}
-		});
+		};
 
+		ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(lootTableSelectorLoader);
 	}
 }
